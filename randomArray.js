8ecb86225695fcfd13b59e4db5d3f4846f27dab5
garygaspar <gary.gaspar@gmail.com>
2023-01-22 00:54:48 +0000

    added comments to exaplain code

--- randomArray.js
+++ randomArray.js
@@ -1,26 +1,25 @@
 // Generate a duplicate-free array of random numbers between 0 & 1000
 
-const amountOutput = 20
+const items = 20
 const range = 1000
-const myArray = setNoDuplicateArray(amountOutput)
+const myArray = setNoDuplicateArray(items)
 
 
 function setNoDuplicateArray (items) {
     const arrayWithDup = []
     const arrayWithoutDup = []
-    if (!(amountOutput > range)){ // to avoid infinite loop
-        while (arrayWithoutDup.length < items) {
-            const num = Math.floor(Math.random() * range) 
-            arrayWithDup.push(num)
-            arrayWithDup.forEach(element => {
-                if (!arrayWithoutDup.includes(element))
+    if (!(items > range)){ // to avoid infinite loop
+        while (arrayWithoutDup.length < items) { // keep looping until dup-free array is init
+            const num = Math.floor(Math.random() * range) // generate random integer
+            arrayWithDup.push(num) // push to temporary array
+            arrayWithDup.forEach(element => { 
+                if (!arrayWithoutDup.includes(element)) // if number is not already in clean array, push it
                     arrayWithoutDup.push(num)
             });
         }
     } else {
-        console.log("amountOutput cannot be greater than range")
+        console.log("items cannot be greater than range")
     }
-
     return arrayWithoutDup
 }
 
