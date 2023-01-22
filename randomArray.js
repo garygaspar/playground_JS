936dc4c091c5629269e1d627b061e0c0af472d3e
garygaspar <gary.gaspar@gmail.com>
2023-01-22 00:44:07 +0000

    protect against infinite loop

--- randomArray.js
+++ randomArray.js
@@ -1,4 +1,4 @@
-// Generate an array of number between 0 & 1000, without duplicates
+// Generate a duplicate-free array of random numbers between 0 & 1000
 
 const amountOutput = 20
 const range = 1000
@@ -8,18 +8,20 @@ const myArray = setNoDuplicateArray(amountOutput)
 function setNoDuplicateArray (items) {
     const arrayWithDup = []
     const arrayWithoutDup = []
-    while (arrayWithoutDup.length < items) {
-        const num = Math.floor(Math.random() * range) 
-        // if amountOutput is larger than range, it creates an infinite loop 
-        arrayWithDup.push(num)
-        arrayWithDup.forEach(element => {
-            if (!arrayWithoutDup.includes(element))
-                arrayWithoutDup.push(num)
-        });
+    if (!(amountOutput > range)){ // to avoid infinite loop
+        while (arrayWithoutDup.length < items) {
+            const num = Math.floor(Math.random() * range) 
+            arrayWithDup.push(num)
+            arrayWithDup.forEach(element => {
+                if (!arrayWithoutDup.includes(element))
+                    arrayWithoutDup.push(num)
+            });
+        }
+    } else {
+        console.log("amountOutput cannot be greater than range")
     }
-    return arrayWithoutDup
-    
 
+    return arrayWithoutDup
 }
 
 console.log(myArray)
